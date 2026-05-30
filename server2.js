91392ef3140a50d513886be74e78366ac3e2ede5
Ritom kumar Hajra <ritomk61@gmail.com>
2026-05-30 19:58:28 +0300

    up

--- server2.js
+++ server2.js
@@ -1,5 +1,11 @@
+// const app = require("./src/app")
+
+// app.listen(3000 , () =>{
+//     console.log("hello your code is running");
+// })
+
 const app = require("./src/app")
 
-app.listen(3000 , () =>{
-    console.log("hello your code is running");
+app.listen(3000,() =>{
+    console.log("your code running in port no 3000")
 })
---
+++ src/app2.js
@@ -0,0 +1,19 @@
+// const app = require ("express")
+
+// const app = express()
+
+// const nodes = []
+
+// app.post("/notes" , (req,rem) => {
+//     console.log(req.body)
+// })
+
+
+
+// module.exports = app
+const app = require("express")
+const app = express()
+
+const nodes = []
+
+module.exports = app
